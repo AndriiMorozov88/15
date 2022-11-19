6d7a76fdcce45dddda4c457e65741fed9e0d3af2
Andrii Morozov <slon88888@ukr.net>
2022-11-19 13:52:12 +0200

    added logic to script

--- 15_script.js
+++ 15_script.js
@@ -1,1 +1,28 @@
-const a = 2;
+const gameFieldArray = new Array(15);
+const gameField = document.querySelector('[data-game-field]');
+gameFieldArray[0] = null;
+function showArray() {
+    for (let j = 0; j < 16; j++) {
+        document.getElementById(j).innerHTML = gameFieldArray[j];
+    }
+}
+for (let i = 0; i < 16; i++) {
+    if (i !== 0) { gameFieldArray[i] = i };
+    const gameElement = document.createElement('button');
+    gameField.append(gameElement);
+    gameElement.classList.add('game-element');
+    gameElement.id = i;
+    gameElement.addEventListener('click', () => {
+        translateArray(gameElement.id);
+        showArray()
+    })
+}
+function translateArray(value) {
+    indexNull = gameFieldArray.indexOf(null);
+    indexElement = value;
+    [gameFieldArray[indexElement], gameFieldArray[indexNull]] = [gameFieldArray[indexNull], gameFieldArray[indexElement]];
+    console.log(gameFieldArray);
+}
+gameFieldArray.sort((a, b) => Math.random() - .5);
+showArray();
+console.log(gameFieldArray);
