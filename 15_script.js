51b83adb4533b722e7b2a1b11c9e619765ab5990
Andrii Morozov <slon88888@ukr.net>
2022-11-24 11:03:59 +0200

    removed styles in win case

--- 15_script.js
+++ 15_script.js
@@ -9,7 +9,7 @@ let start = false;
 let stepAmount = 0;
 stepParagraph.innerHTML = `Steps: ${stepAmount}`;
 gameFieldArray[0] = null;
-function getPossibleTurn(index) {
+function getPossibleTurnIndex(index) {
     const possibleTurnArray = [[1, 4], [0, 2, 5], [1, 3, 6], [2, 7], [0, 5, 8], [1, 4, 6, 9], [2, 5, 7, 10], [3, 6, 11], [4, 9, 12], [5, 8, 10, 13], [6, 9, 11, 14], [7, 10, 15], [8, 13], [9, 12, 14], [10, 13, 15], [11, 14]];
     return possibleTurnArray[index];
 };
@@ -19,7 +19,7 @@ function showArray() {
         document.getElementById(count).innerHTML = gameFieldArray[count];
         document.getElementById(count).classList.remove('game-element--possible', 'game-element--null');
     };
-    getPossibleTurn(indexNull).forEach((element) => {
+    getPossibleTurnIndex(indexNull).forEach((element) => {
         document.getElementById(element).classList.add('game-element--possible');
         document.getElementById(indexNull).classList.add('game-element--null');
     });
@@ -52,6 +52,10 @@ for (let count = 0; count < 16; count++) {
             if (checkWinCombination()) {
                 win.classList.remove('game-state__paragraph--invisible');
                 start = null;
+                getPossibleTurnIndex(indexNull).forEach((element) => {
+                    document.getElementById(element).classList.remove('game-element--possible');
+                    document.getElementById(indexNull).classList.remove('game-element--null');
+                });
             }
         } else if (start === false) {
             let flashColor = [
